ce9be9609b470f306d52fe288caaf49eb893a7b1
Bamboo6 <dream1718@foxmail.com>
2017-10-27 14:59:43 +0800

    set UIButton&UILabel

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
+    
+    @IBOutlet weak var lab: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn.addTarget(self, action: #selector(btnclick), for: .touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func btnclick() {
+        lab.text="哈哈哈，我是一个文本"
+    }
 
 }
 
